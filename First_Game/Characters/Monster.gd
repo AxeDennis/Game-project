b5919c018d48d518174c3ce837f62101391bac95
AxeDennis <59689963+AxeDennis@users.noreply.github.com>
2020-03-23 17:38:26 +0100

    Update 2020-03-23 17:36
    Got some work done on the monster, it now follows a set path and chases the player if the player comes too close. Still need to fix pathfinding so the monster dont get stuck on objects etc.

---
+++ First_Game/Characters/Monster.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+onready var hide = get_node("../../Player")
+
+var state = 0
+var move_speed
+export (NodePath) var patrol_path
+var patrol_points
+var patrol_index = 5
+var velocity = Vector2.ZERO
+var player = null
+
+func _ready():
+	if patrol_path:
+		patrol_points = get_node(patrol_path).curve.get_baked_points()
+
+func _physics_process(_delta):
+	if state == 0:
+		move_speed = 100
+		var target = patrol_points[patrol_index]
+		if position.distance_to(target) <= 1:
+        	patrol_index = wrapi(patrol_index + 1, 0, patrol_points.size())
+        	target = patrol_points[patrol_index]
+		velocity = (target - position).normalized() * move_speed
+		velocity = move_and_slide(velocity)
+	if state == 1:
+		move_speed = 150
+		player
+		velocity = position.direction_to(player.position) * move_speed
+		velocity = move_and_slide(velocity)
+		if hide.Hide == 2:
+			state = 0
+
+
+func _on_Detectradius_body_entered(body):
+	if body == get_node("../../Player"):
+		state = 1
+		player = body
+
+func _on_Detectradius_body_exited(body):
+	if body == get_node("../../Player"):
+		state = 0
+		player = null
