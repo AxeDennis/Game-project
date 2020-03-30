8f580c99af36505903b6f6b0ef0f79126ac58b64
AxeDennis <59689963+AxeDennis@users.noreply.github.com>
2020-03-30 17:22:36 +0200

    Update 2020-03-30 17:21
    Worked on the pathfinding for the monster, almost working. Getting some errors i need to fix.

--- First_Game/Characters/Monster.gd
+++ First_Game/Characters/Monster.gd
@@ -9,28 +9,54 @@ var patrol_points
 var patrol_index = 5
 var velocity = Vector2.ZERO
 var player = null
+var path : = PoolVector2Array() setget set_path
 
 func _ready():
 	if patrol_path:
 		patrol_points = get_node(patrol_path).curve.get_baked_points()
-		#vector2get_closest_point and poolvector2array
-func _physics_process(delta):
+		
+func _process(delta: float) -> void:
+	rotation = velocity.angle()
 	if state == 0:
 		move_speed = 50
 		var target = patrol_points[patrol_index]
 		if position.distance_to(target) <= 1:
-        	patrol_index = wrapi(patrol_index + 1, 0, patrol_points.size())
-        	target = patrol_points[patrol_index]
+			patrol_index = wrapi(patrol_index + 1, 0, patrol_points.size())
+			target = patrol_points[patrol_index]
 		velocity = (target - position).normalized() * move_speed
 		velocity = move_and_slide(velocity)
 	if state == 1:
+		var move_distance : = move_speed * delta
+		move_along_path(move_distance)
 		move_speed = 150
-		player
-		velocity = position.direction_to(player.position) * move_speed
-		velocity = move_and_slide(velocity)
+#		player
+#		velocity = position.direction_to(player.position) * move_speed
+#		velocity = move_and_slide(velocity)
 		if hide.Hide == 2:
 			state = 0
-
+			
+func move_along_path(distance : float) -> void:
+	var start_point : = position 
+	for i in range(path.size()):
+		var distance_to_next : = start_point.distance_to(path[0])
+		if distance <= distance_to_next and distance >= 0.0:
+			position = start_point.linear_interpolate(path[0], distance / distance_to_next)
+			break
+		elif distance < 0.0:
+			position = path[0]
+			break
+		distance -= distance_to_next
+		start_point = path[0]
+		path.remove(0)
+		
+func set_path(value : PoolVector2Array) -> void:
+		path = value
+		if value.size() == 0:
+			return
+		set_process(true)
+		
+		
+		
 func _on_Detectradius_body_entered(body):
 	if body == get_node("../../Player"):
 		state = 1
--- First_Game/Maps/Map01.gd
+++ First_Game/Maps/Map01.gd
@@ -1,1 +1,11 @@
 extends "res://Maps/Map.gd"
+
+onready var nav_2d : Navigation2D = $Navigation2D
+onready var line_2d : Line2D = $Line2D
+onready var monster = get_node("Characters/Monsters/Monster")
+onready var player = get_node("Characters/Player")
+
+func _process(delta):
+	var new_path : = nav_2d.get_simple_path(monster.global_position, player.global_position)
+	line_2d.points = new_path
+	monster.path = new_path
