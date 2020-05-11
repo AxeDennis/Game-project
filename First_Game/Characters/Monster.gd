faa559fd2b4c3c6823cb81a4b0f2d80f5d88389f
AxeDennis <59689963+AxeDennis@users.noreply.github.com>
2020-05-11 15:44:03 +0200

    Update 2020-05-11

--- First_Game/Characters/Monster.gd
+++ First_Game/Characters/Monster.gd
@@ -1,8 +1,7 @@
 extends KinematicBody2D
-
+		
 onready var hide = get_node("../../Player")
 onready var test = get_node("../../..")
-
 var state = 0
 var move_speed
 export (NodePath) var patrol_path
@@ -11,48 +10,37 @@ var patrol_index = 5
 var velocity = Vector2.ZERO
 var player = null
 var path = PoolVector2Array() setget set_path
-
+		
 func _ready():
 	if patrol_path:
 		patrol_points = get_node(patrol_path).curve.get_baked_points()
 		
 func _process(delta: float) -> void:
-	
+		
 	rotation = velocity.angle()
 	if state == 0:
 		move_speed = 50
 		var target = patrol_points[patrol_index]
+		
 		if position.distance_to(target) <= 1:
 			patrol_index = wrapi(patrol_index + 1, 0, patrol_points.size())
 			target = patrol_points[patrol_index]
-		velocity = (target - position).normalized() * move_speed
+		test._set_target(target)
+		velocity = (path[1] - position).normalized() * move_speed
 		move_and_slide(velocity)
 	if state == 1:
 		move_speed = 150
+		path = player
 		var target = path[0]
 		if position.distance_to(target) <= 1:
 			target = path[1]
 		velocity = (target - position).normalized() * move_speed
 		move_and_slide(velocity)
-#		var move_distance : int = move_speed * delta
-#		move_along_path(move_distance)
+		
 		if hide.Hide == 2:
 			state = 0
-			
-func move_along_path(distance : float) -> void:
-	var start_point = position 
-	for i in range(path.size()):
-		var distance_to_next = start_point.distance_to(path[0])
-		if distance <= distance_to_next and distance >= 0.0:
-			position = start_point.linear_interpolate(path[0], distance * distance_to_next)
-			break
-		elif distance < 0.0:
-			position = path[0]
-			set_process(false)
-			break
-		distance -= distance_to_next
-		start_point = path[0]
-		path.remove(0)
+		
+		
 		
 func set_path(value : PoolVector2Array) -> void:
 		path = value
@@ -66,7 +54,8 @@ func _on_Detectradius_body_entered(body):
 	if body == get_node("../../Player"):
 		state = 1
 		player = body
-
+		
+		
 func _on_Detectradius_body_exited(body):
 	if body == get_node("../../Player"):
 		state = 0
